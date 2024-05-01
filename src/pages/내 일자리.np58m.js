68dfa54c40e545c70d67bd647cfb3d15cfad0c18
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2024-05-01 18:50:44 +0900

    update my jobs

--- src/pages/내 일자리.np58m.js
+++ src/pages/내 일자리.np58m.js
@@ -7,4 +7,18 @@ $w.onReady(function () {
     // To select an element by ID use: $w('#elementID')
 
     // Click 'Preview' to run your code
+    $w('#button8').onMouseIn( (event) => {
+        const clickedElement = event.target;
+        clickedElement.style.color = "#FD5521";
+        clickedElement.style.borderColor = "#FD5521";
+        console.log(clickedElement.id);
+    })
+
+    $w('#button8').onMouseOut( (event) => {
+        const clickedElement = event.target;
+        clickedElement.style.color = "#C7C7C7";
+        clickedElement.style.borderColor = "#C7C7C7";
+        console.log(clickedElement.id);
+    })
+
 });
