8a1d00ca8ad64588cbfdd6cde455d26c91da7679
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2024-05-28 22:28:27 +0900

    link data

--- src/pages/구인공고 (List).m5dvr.js
+++ src/pages/구인공고 (List).m5dvr.js
@@ -1,39 +1,48 @@
 import { getDataWithGetMethod } from "backend/dataFetcher";
     //  content body
-    //   {
-    //     "jobPostId": 1,
-    //     "tech": "NORMAL",
-    //     "recruitNum": 60,
-    //     "title": "사하구  낙동5블럭  낙동강 온도 측정 센터 신축공사",
-    //     "meal": true,
-    //     "pickup": true,
-    //     "park": "FREE",
-    //     "startDate": "2024-07-01",
-    //     "endDate": "2024-07-02",
-    //     "startTime": "18:00:00",
-    //     "endTime": "18:00:00",
-    //     "address": "부산광역시 사하구 낙동대로 550번길 37",
-    //     "distance": null,
-    //     "companyName": "삼성",
-    //     "wage": 150000,
-    //     "isScrap": null,
-    //     "thumbnailS3Url": "https://jikgong-resize-bucket.s3.ap-northeast-2.amazonaws.com/jobPost/thumbnail_3c8f0d5f-7fce-4c12-8b4f-475dedafe7db.jpg"
-    //   },
+      // {
+      //   "jobPostId": 1,
+      //   "tech": "NORMAL",
+      //   "recruitNum": 60,
+      //   "title": "사하구  낙동5블럭  낙동강 온도 측정 센터 신축공사",
+      //   "meal": true,
+      //   "pickup": true,
+      //   "park": "FREE",
+      //   "startDate": "2024-07-01",
+      //   "endDate": "2024-07-02",
+      //   "startTime": "18:00:00",
+      //   "endTime": "18:00:00",
+      //   "address": "부산광역시 사하구 낙동대로 550번길 37",
+      //   "distance": null,
+      //   "companyName": "삼성",
+      //   "wage": 150000,
+      //   "isScrap": null,
+      //   "thumbnailS3Url": "https://jikgong-resize-bucket.s3.ap-northeast-2.amazonaws.com/jobPost/thumbnail_3c8f0d5f-7fce-4c12-8b4f-475dedafe7db.jpg"
+      // },
 
 $w.onReady(async function () {
   // 기존 데이터 초기화 + 데이터 받아오기
-  initRepeater()
-  initData()
+  $w('#listRepeater').data = []
+  initComponents()
+  render()
   // 데이터 할당 시작
 });
 
-async function initData(){
+function initComponents() {
+  initRepeater()
+}
+
+async function render(){
     $w("#listRepeater").data = []
     var { data, message } = await getDataWithGetMethod({
     url: "http://43.203.86.121/api/job-post/worker/list",
   });
-  console.log(data.content);
-  $w("#listRepeater").data = data;
+  for(let i=0;i < data.content.length;i++) {
+    data.content[i]._id = `${i+1}`
+  }
+  console.log(data.content)
+  $w("#listRepeater").data = []
+  $w("#listRepeater").data = data.content;
 }
 
 function initRepeater() {
@@ -53,7 +62,11 @@ function initItemBackground($item, itemData) {
 }
 
 function initItemSelectionTag($item, itemData) {
-  $item("#selectionTags5").elipsisText = itemData.meal
+  console.log(itemData.meal, itemData.pickup)
+  $item("#selectionTags5").options = [
+    {'label':'meal','value':`${itemData.meal}`}, 
+    {'label':'pickup','value':`${itemData.pickup}`}
+  ]
 }
 
 // function initItemDescription($item, itemData, itemIndex) {
