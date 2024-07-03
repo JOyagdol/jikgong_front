ca3fd2b049e665b3bedcb43747f96451a9666bf2
rvjst <90255680+rvjst@users.noreply.github.com>
2024-07-03 18:13:02 +0900

    로그인 추가 수정

--- src/pages/로그인.t0nlg.js
+++ src/pages/로그인.t0nlg.js
@@ -1,7 +1,8 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 import { fetch, getJSON } from 'wix-fetch';
-import { setToken } from "backend/login";
+import { setToken, getToken } from "backend/login";
+import wixWindowFrontend from "wix-window-frontend";
 
 $w.onReady(function () {
     // Write your JavaScript here
@@ -9,10 +10,15 @@ $w.onReady(function () {
     // To select an element by ID use: $w('#elementID')
 
     $w('#button18').onClick(async () => {
+        const id = $w('#input3').value;
+        const password = $w('#input4').value;
+
+        console.log(id)
+        console.log(password)
         const loginUrl = "https://asdfdsas.p-e.kr/api/login"
         const data = {
-            loginId: "abcdefg1",
-            password: "abcdefg1",
+            loginId: id,
+            password: password,
             deviceToken: "string"
         }
         try {
@@ -29,13 +35,16 @@ $w.onReady(function () {
               const responseData = await loginResponse.json()
               console.log(responseData.data.accessToken)
               await setToken(responseData.data.accessToken)
+              const token = await getToken(responseData.data.accessToken);
+              console.log('Retrieved token:', token);
+              wixWindowFrontend.lightbox.close();
         }
         catch (error) {
             console.log('Error:',error)
         }
-        
-       
-
     })
     // Click 'Preview' to run your code
 });
+
+
+
