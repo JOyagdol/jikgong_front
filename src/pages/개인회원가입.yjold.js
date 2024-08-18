aa83482210aa957b86baab5e4b7c4a1f348e7fe4
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2024-08-19 03:02:34 +0900

    Update 개인회원가입.yjold.js

--- src/pages/개인회원가입.yjold.js
+++ src/pages/개인회원가입.yjold.js
@@ -1,9 +1,12 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
+import wixLocation from 'wix-location-frontend';
+
 var checkPhoneCode = "false";
 var checkRepeatPhone = "false";
 var checkRepeatId = "false";
+var checkJoinData = "true";
 var authPhoneCode;
 
 var joinData = {}
@@ -57,7 +60,6 @@ $w.onReady(function () {
                 body: JSON.stringify(data)
             })
             const responsePhoneData = await phoneResponse.json()
-            console.log(responsePhoneData)
             if(responsePhoneData.message == "사용 가능한 휴대폰 입니다.") {
                 $w("#text154").show()
                 $w("#text154").text = "인증되었습니다."
@@ -97,9 +99,7 @@ $w.onReady(function () {
 
 
 
-    $w("#button21").onClick(() => {
-        
-        
+    $w("#button21").onClick(async () => {
         let password = $w("#input11").value;
         let repassword = $w("#input12").value;
 
@@ -122,15 +122,79 @@ $w.onReady(function () {
         else {
             joinData.password = password
             joinData.workerName = $w("#input3").value
+            if(!joinData.workerName) {
+                checkJoinData = "false"
+                $w("#button21").label = "이름을 입력해주세요."
+            }
             joinData.role = "ROLE_WORKER"
 
             joinData.deviceToken = "token"
-            joinData.isNotification = "true"
+            joinData.isNotification = true
             let birthDate = $w("#datePicker1").value
-            joinData.birth = formatDate(birthDate)            
+            joinData.birth = formatDate(birthDate)
 
+            joinData.nationality = $w("#dropdown1").value 
+            if(!joinData.nationality) {
+                checkJoinData = "false"
+                $w("#button21").label = "국적을 선택해주세요."
+            }
+            
+            let gender = $w("#selectionTags1").value[0]
+            joinData.gender = gender
+            if(!joinData.gender) {
+                checkJoinData = "false"
+                $w("#button21").label = "성별을 선택해주세요."
+            }
+            
+            let address = $w("#addressInput1").value
+            if(!joinData.address) {
+                checkJoinData = "false"
+                $w("#button21").label = "주소를 입력해주세요."
+            }
+            joinData.address = address.formatted
+            joinData.latitude = address.location.latitude
+            joinData.longitude = address.location.longitude
+        
+            let rrn = $w("#input7").value + "-" + $w("#input8").value
+            joinData.rrn = rrn
+            if(!joinData.rrn) {
+                checkJoinData = "false"
+                $w("#button21").label = "주민등록번호를 입력해주세요."
+            }
+           
+            joinData.bank = $w("#dropdown2").value
+            if(!joinData.bank) {
+                checkJoinData = "false"
+                $w("#button21").label = "은행을 선택해주세요."
+            }
+            joinData.account = $w("#input9").value
+            if(!joinData.account) {
+                checkJoinData = "false"
+                $w("#button21").label = "계좌번호를 입력해주세요."
+            }
 
-            // 회원가입 패치
+            if(checkJoinData == "true") {
+                const joinUrl = "https://asdfdsas.p-e.kr/api/join/worker/join"
+                const joinResponse = await fetch(joinUrl, {
+                    method: "POST",
+                    headers: {
+                        'Content-Type' : 'application/json',
+                    },
+                    body: JSON.stringify(joinData)
+                })
+                const responseJoinData = await joinResponse.json()
+    
+                if (responseJoinData.message == "노동자 회원 가입 완료") {
+                    $w("#button21").label = responseJoinData.message
+                    wixLocation.to(`/로그인`);
+                }
+                else {
+                    $w("#button21").label = "회원 가입 오류 : 재작성 및 재시도 부탁드립니다."
+                }
+            }
+            else {
+                $w("#button21").label = "회원 정보를 올바르게 입력해주세요."
+            }
         }
     })
 });
