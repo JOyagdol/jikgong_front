7fe71b9f7e1f6b7592de9063523cad1fc61add00
JOyagdol <pool021224@gmail.com>
2024-09-26 14:13:40 +0900

    Update 문의하기.vwdio.js

--- src/pages/문의하기.vwdio.js
+++ src/pages/문의하기.vwdio.js
@@ -7,26 +7,26 @@ import wixLocation from 'wix-location-frontend';
 
 $w.onReady(function () {
     // Write your JavaScript here
-    var loginKey = session.getItem("loginKey");
-    if(loginKey) {
-        $w("#button4").label = "로그아웃"
-        $w("#button4").onClick(() => {
-            session.removeItem("loginKey");
-            $w("#button4").label = "로그인"
-            wixLocation.to(`/`);
-        })
+    // var loginKey = session.getItem("loginKey");
+    // if(loginKey) {
+    //     $w("#button4").label = "로그아웃"
+    //     $w("#button4").onClick(() => {
+    //         session.removeItem("loginKey");
+    //         $w("#button4").label = "로그인"
+    //         wixLocation.to(`/`);
+    //     })
 
-        $w("#button21").onClick(() => {
-            const name = $w("#input1").value
-            const email = $w("#input2").value
-            const message = $w("#textBox1").value
-            sendEmail(name, email, message)
-                .then(response => console.log(response))
-        })
-    }
-    else {
-        $w("#text31").text = "로그인 후 이용 가능합니다."
-    }
+    //     $w("#button21").onClick(() => {
+    //         const name = $w("#input1").value
+    //         const email = $w("#input2").value
+    //         const message = $w("#textBox1").value
+    //         sendEmail(name, email, message)
+    //             .then(response => console.log(response))
+    //     })
+    // }
+    // else {
+    //     $w("#text31").text = "로그인 후 이용 가능합니다."
+    // }
 
     // To select an element by ID use: $w('#elementID')
 
