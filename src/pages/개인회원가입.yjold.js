0375cd8f545c2e35d4d1c20281504b1e0362d969
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2024-10-06 01:10:19 +0900

    update

--- src/pages/개인회원가입.yjold.js
+++ src/pages/개인회원가입.yjold.js
@@ -311,5 +311,5 @@ function formatDate(date) {
     const month = ('0' + (date.getMonth() + 1)).slice(-2); 
     const day = ('0' + date.getDate()).slice(-2); 
 
-    return `${year}${month}${day}`;
+    return `${year}-${month}-${day}`;
 }
--- src/pages/내 정보.pe28q.js
+++ src/pages/내 정보.pe28q.js
@@ -7,6 +7,9 @@ import { session } from 'wix-storage-frontend';
 var loginKey = session.getItem("loginKey");
 let tech_list = {"NORMAL":"보통인부","FOREMAN":"작업반장","SKILLED_LABORER":"특별인부","HELPER":"조력공","SCAFFOLDER":"비계공","FORMWORK_CARPENTER":"형틀목공","REBAR_WORKER":"철근공","STEEL_STRUCTURE_WORKER":"철골공","WELDER":"용접공","CONCRETE_WORKER":"콘크리트공","BRICKLAYER":"조적공","DRYWALL_FINISHER":"견출공","CONSTRUCTION_CARPENTER":"건축목공","WINDOW_DOOR_INSTALLER":"창호공","GLAZIER":"유리공","WATERPROOFING_WORKER":"방수공","PLASTERER":"미장공","TILE":"타일","PAINTER":"도장공","INTERIOR_FINISHER":"내장공","WALLPAPER_INSTALLER":"도배공","POLISHER":"연마공","STONEMASON":"석공","GROUT_WORKER":"줄눈공","PANEL_ASSEMBLER":"판넬조립공","ROOFER":"지붕잇기공","LANDSCAPER":"조경공","CAULKER":"코킹공","PLUMBER":"배관공","BOILER_TECHNICIAN":"보일러공","SANITARY_TECHNICIAN":"위생공","DUCT_INSTALLER":"덕트공","INSULATION_WORKER":"보온공","MECHANICAL_EQUIPMENT_TECHNICIAN":"기계설비공","ELECTRICIAN":"내선전공","TELECOMMUNICATIONS_INSTALLER":"통신내선공","TELECOMMUNICATIONS_EQUIPMENT_INSTALLER":"통신설비공"};
 
+var workExperienceRequest = []
+var joinData = {}
+
 $w.onReady(async function () {
 
     $w("#button25").onClick(async () => {
@@ -17,11 +20,18 @@ $w.onReady(async function () {
             'value':tag,
             'label':tag
         })
+        workExperienceRequest.push({
+            "workExperienceId":null, //?
+            "tech":result.tech,
+            "experienceMonths":result.year*12 + result.month
+        })
+
         $w("#selectionTags2").options = workExperienceTag
 
     })
 
     $w("#button26").onClick(() => {
+        workExperienceRequest = []
         workExperienceTag = []
         $w("#selectionTags2").options = workExperienceTag
     })
@@ -39,6 +49,7 @@ $w.onReady(async function () {
         })
 
     var responseData = await myInfoResponse.json()
+    console.log(responseData)
     
     //personal info
     $w("#input1").value = responseData.data.phone;
@@ -49,7 +60,13 @@ $w.onReady(async function () {
 
     $w("#dropdown1").value = responseData.data.nationality;
 
-    $w("#selectionTags1").value = responseData.data.gender;
+    
+    if(responseData.data.gender == "MALE") {
+        $w("#selectionTags1").selectedIndices = [0,0]
+    }
+    else {
+        $w("#selectionTags1").selectedIndices = [0,1]
+    }
 
     //주소 표시
     let addressUrl = "https://asdfdsas.p-e.kr/api/location/list"
@@ -62,6 +79,7 @@ $w.onReady(async function () {
         })
     
     var addressResponseData = await addressResponse.json();
+    console.log(addressResponseData.data)
     for(let i=0;i<addressResponseData.data.length;i++) {
         if(addressResponseData.data[i].isMain == true) {
             $w("#addressInput1").placeholder = addressResponseData.data[i].address
@@ -70,6 +88,7 @@ $w.onReady(async function () {
 
     //workExperience
     var workExperienceTag = []
+    workExperienceRequest = responseData.data.workExperienceResponseList
     for(let i=0;i<responseData.data.workExperienceResponseList.length;i++) {
         let tech = responseData.data.workExperienceResponseList[i].tech
         let d = Number(responseData.data.workExperienceResponseList[i].experienceMonths)
@@ -98,12 +117,73 @@ $w.onReady(async function () {
         $w("#checkbox3").checked = true
     }
 
-    $w("#button21").onClick(() => {
+    $w("#button21").onClick(async () => {
         // 개인 정보 수정
+        joinData.workerName = $w("#input3").value;
+        let birthDate = $w("#datePicker1").value
+        joinData.birth = formatDate(birthDate);
+        joinData.rrn = formatRrn(birthDate);
+        joinData.gender = $w("#selectionTags1").value[0]
+        joinData.nationality = $w("#dropdown1").value
+        joinData.hasVisa = $w("#checkbox1").checked
+        joinData.hasEducationCertificate = $w("#checkbox2").checked
+        joinData.hasWorkerCard = $w("#checkbox3").checked
+
+        joinData.workExperienceRequestList = workExperienceRequest;
+
+        console.log(joinData);
+
+        const myInfoPutResponse = await fetch(myInfoUrl, {
+            method: "PUT",
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization': `Bearer ${loginKey}`
+                },
+            body: JSON.stringify(joinData)
+            })
+    
+        var myInfoPutData = await myInfoPutResponse.json()
+        console.log(myInfoPutData)
 
 
         // 주소 수정
+        let address = $w("#addressInput1").value
+        var postLocation = {}
+        if(address) {
+            postLocation.address = address.formatted
+            postLocation.latitude = address.location.latitude
+            postLocation.longitude = address.location.longitude
+            postLocation.isMain = true
+
+            const locationUrl = "https://asdfdsas.p-e.kr/api/location"
+            const locationResponse = await fetch(locationUrl, {
+                method: "POST",
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'Authorization': `Bearer ${loginKey}`
+                    },
+                body: JSON.stringify(postLocation)
+            })
+            var locationResponseData = await locationResponse.json()
+            console.log(locationResponseData)
+        }
 
     })
     
 });
+
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); 
+    const day = ('0' + date.getDate()).slice(-2); 
+
+    return `${year}-${month}-${day}`;
+}
+
+function formatRrn(date) {
+    const year = String(date.getFullYear()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); 
+    const day = ('0' + date.getDate()).slice(-2); 
+
+    return `${year}${month}${day}`;
+}
