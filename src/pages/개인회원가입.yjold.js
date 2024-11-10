27929398a371930ab7d25a2fb0258918d58e7328
JOyagdol <86817044+JOyagdol@users.noreply.github.com>
2024-11-11 06:53:53 +0900

    update

--- src/pages/개인회원가입.yjold.js
+++ src/pages/개인회원가입.yjold.js
@@ -33,6 +33,10 @@ $w.onReady(function () {
     $w("#text171").collapse()
     $w("#line10").collapse()
     $w("#checkbox1").collapse()
+
+    $w("#section2").collapse()
+    $w("#section3").collapse()
+    $w("#section4").collapse()
     
     $w("#dropdown1").onChange(() => {
         if($w("#dropdown1").value == "외국인") {
@@ -200,21 +204,101 @@ $w.onReady(function () {
         }
     })
 
-
-
-    $w("#button21").onClick(async () => {
-        let password = $w("#input11").value;
-        let repassword = $w("#input12").value;
-
+    $w("#button21").onClick((event) => {
         if(checkPhoneCode == "false") {
             $w("#text154").show()
             $w("#text154").text = "전화번호 인증해야 합니다."
+            return false;
         }
         else if(checkRepeatPhone == "false") {
             $w("#text154").show()
             $w("#text154").text = "이미 가입하신 전화번호 입니다."
+            return false;
         }
-        else if(checkRepeatId == "false") {
+        joinData.workerName = $w("#input3").value
+        if(!joinData.workerName) {
+            checkJoinData = "false"
+            $w("#button21").label = "이름을 입력해주세요."
+            return false;
+        }
+        joinData.role = "ROLE_WORKER"
+
+        joinData.deviceToken = "token"
+        joinData.isNotification = true
+        let birthDate = $w("#datePicker1").value
+        joinData.birth = formatDate(birthDate)
+
+        joinData.nationality = $w("#dropdown1").value 
+        if(!joinData.nationality) {
+            checkJoinData = "false"
+            $w("#button21").label = "국적을 선택해주세요."
+            return false;
+        }
+        
+        let gender = $w("#selectionTags1").value[0]
+        joinData.gender = gender
+        if(!joinData.gender) {
+            checkJoinData = "false"
+            $w("#button21").label = "성별을 선택해주세요."
+            return false;
+        }
+        
+        let address = $w("#addressInput1").value
+        
+        if(!address) {
+            checkJoinData = "false"
+            $w("#button21").label = "주소를 입력해주세요."
+            return false;
+        }
+        joinData.address = address.formatted
+        joinData.latitude = address.location.latitude
+        joinData.longitude = address.location.longitude
+    
+        let rrn = $w("#input7").value + "-" + $w("#input8").value
+        joinData.rrn = rrn
+        if(!$w("#input7").value || !$w("#input8").value) {
+            checkJoinData = "false"
+            $w("#button21").label = "주민등록번호를 입력해주세요."
+            return false;
+        }
+
+        $w("#section1").collapse()
+        $w("#section2").expand()
+
+    })
+
+    $w("#button27").onClick(() => {
+
+        joinData.workExperienceRequest = workExperienceRequest;
+        $w("#section2").collapse()
+        $w("#section3").expand()
+    })
+
+    $w("#button28").onClick(() => {
+        joinData.bank = $w("#dropdown2").value
+        if(!joinData.bank) {
+            checkJoinData = "false"
+            $w("#button28").label = "은행을 선택해주세요."
+            return false;
+        }
+        joinData.account = $w("#input9").value
+        if(!joinData.account) {
+            checkJoinData = "false"
+            $w("#button28").label = "계좌번호를 입력해주세요."
+            return false;
+        }
+
+        $w("#section3").collapse()
+        $w("#section4").expand()
+    })
+
+
+    $w("#button30").onClick(async () => {
+        let password = $w("#input11").value;
+        let repassword = $w("#input12").value;
+
+        
+        if(checkRepeatId == "false") {
             $w("#text167").show()
             $w("#text167").text = "중복된 ID 입니다."
         }
@@ -224,60 +308,7 @@ $w.onReady(function () {
         }
         else {
             joinData.password = password
-            joinData.workerName = $w("#input3").value
-            if(!joinData.workerName) {
-                checkJoinData = "false"
-                $w("#button21").label = "이름을 입력해주세요."
-            }
-            joinData.role = "ROLE_WORKER"
-
-            joinData.deviceToken = "token"
-            joinData.isNotification = true
-            let birthDate = $w("#datePicker1").value
-            joinData.birth = formatDate(birthDate)
-
-            joinData.nationality = $w("#dropdown1").value 
-            if(!joinData.nationality) {
-                checkJoinData = "false"
-                $w("#button21").label = "국적을 선택해주세요."
-            }
             
-            let gender = $w("#selectionTags1").value[0]
-            joinData.gender = gender
-            if(!joinData.gender) {
-                checkJoinData = "false"
-                $w("#button21").label = "성별을 선택해주세요."
-            }
-            
-            let address = $w("#addressInput1").value
-            
-            if(!address) {
-                checkJoinData = "false"
-                $w("#button21").label = "주소를 입력해주세요."
-            }
-            joinData.address = address.formatted
-            joinData.latitude = address.location.latitude
-            joinData.longitude = address.location.longitude
-        
-            let rrn = $w("#input7").value + "-" + $w("#input8").value
-            joinData.rrn = rrn
-            if(!joinData.rrn) {
-                checkJoinData = "false"
-                $w("#button21").label = "주민등록번호를 입력해주세요."
-            }
-           
-            joinData.bank = $w("#dropdown2").value
-            if(!joinData.bank) {
-                checkJoinData = "false"
-                $w("#button21").label = "은행을 선택해주세요."
-            }
-            joinData.account = $w("#input9").value
-            if(!joinData.account) {
-                checkJoinData = "false"
-                $w("#button21").label = "계좌번호를 입력해주세요."
-            }
-
-            joinData.workExperienceRequest = workExperienceRequest;
 
             joinData.hasVisa = hasVisa;
             joinData.hasEducationCertificate = hasEducationCertificate;
@@ -287,9 +318,9 @@ $w.onReady(function () {
 
             if(!joinData.privacyConsent) {
                 checkJoinData = "false"
-                $w("#button21").label = "개인정보 수집 동의해주세요."
+                $w("#button30").label = "개인정보 수집 동의해주세요."
             }
-
+            console.log(checkJoinData)
             if(checkJoinData == "true") {
                 const joinUrl = "https://asdfdsas.p-e.kr/api/join/worker/join"
                 const joinResponse = await fetch(joinUrl, {
@@ -302,11 +333,11 @@ $w.onReady(function () {
                 const responseJoinData = await joinResponse.json()
                 console.log(responseJoinData)
                 if (responseJoinData.message == "노동자 회원 가입 완료") {
-                    $w("#button21").label = responseJoinData.message
+                    $w("#button30").label = responseJoinData.message
                     wixWindow.openLightbox("회원가입");
                 }
                 else {
-                    $w("#button21").label = "회원 가입 오류 : 재작성 및 재시도 부탁드립니다."
+                    $w("#button30").label = "회원 가입 오류 : 재작성 및 재시도 부탁드립니다."
                 }
             }
         }
